105a6d7c4a588ac9957e7aba4729c0849b54227e
Ethan Chen <intervigil@gmail.com>
2018-10-02 13:51:03 +0200

    mido: Use Soong namespace to avoid device build conflicts
    Change-Id: Id04f464102264b6f2e8d49b39389c7e346613b39

--- device.mk
+++ device.mk
@@ -24,6 +24,9 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-mokee
 # Inherit from msm8953-common
 $(call inherit-product, device/xiaomi/msm8953-common/msm8953.mk)
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += device/xiaomi/mido
+
 # Audio configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
