d1315b37baba794125dc3ae7010468c4b16d24f4
Dede Dindin Qudsy <xtrymind@gmail.com>
2018-11-09 14:59:15 +0700

    mido: copy permission for ir and fingerprint
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- device.mk
+++ device.mk
@@ -37,6 +37,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-service.xiaomi_mido
 
+# Fingerprint and Consumerir
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
+    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.mido.rc \
