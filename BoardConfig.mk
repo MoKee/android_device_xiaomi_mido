8ac0d814de6cc93264ac9e81aa8ac380541eb602
Anderson Mendes <anderson2000ano@gmail.com>
2019-06-14 17:15:27 -0300

    mido: Set kernel path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,6 +31,7 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Kernel
 TARGET_KERNEL_CONFIG := mido_defconfig
+TARGET_KERNEL_SOURCE := kernel/xiaomi/mido
 
 # Partitions
 BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
