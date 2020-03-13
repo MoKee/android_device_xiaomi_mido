f9ecec6150ce3bbd9516b6da300369fcf5965aeb
Cyborg2017 <wangyang0987654@gmail.com>
2020-03-13 22:33:48 +0800

    mido: Add device maintainer

--- prop.mk
+++ prop.mk
@@ -4,3 +4,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.audio.fluence.voicecall=true \
     persist.vendor.audio.fluence.voicerec=true \
     ro.vendor.audio.sdk.fluencetype=fluence
+
+# Maintainer
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.mk.maintainer=Cyborg2017
