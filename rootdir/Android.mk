886c1479c0f963dd7e290a4db4fcb2a41c15ef8b
Cyborg2017 <wangyang0987654@gmail.com>
2020-03-13 22:09:38 +0800

    mido: rootdir: drop deprecated local module tag

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 # Device Script
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.mido.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.mido.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
@@ -11,7 +11,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.goodix.sh
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.goodix.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
